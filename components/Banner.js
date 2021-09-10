e5affdfbc21619f2d4f2af99ad297b9d2ad71a75
Revolttt <30984354+Revolttt@users.noreply.github.com>
2021-09-10 11:21:56 +0200

    updated banner button

--- components/Banner.js
+++ components/Banner.js
@@ -7,11 +7,11 @@ const Banner = () => {
             <Image src="/images/banner.png" layout="fill" objectFit="cover" />
             <div className="absolute top-1/3 w-full text-center">
                 <p className="font-bold text-sm sm:text-lg">
-                    Ne znaš što točno tražiš?
+                    Prodaješ nekretninu? 
                 </p>
                 <button className="bg-white px-10 py-4 shadow-md hover:shadow-2xl active:scale-90 transition duration-150 rounded-full my-3">
                     <div className=" font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-700">
-                        Pogledaj nove oglase
+                        Objavi oglas
                     </div>
                 </button>
             </div>
