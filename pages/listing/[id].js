2c5b14a36f62472139059f48e522a1a223bedcb1
Marko <marko.bolt4@gmail.com>
2021-12-02 17:58:59 +0100

    styling listing page

--- pages/listing/[id].js
+++ pages/listing/[id].js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import Image from "next/image"
+import { PhoneIcon, MailIcon } from "@heroicons/react/solid"
 import { getAllListingIds, getListingData } from "../../dummy/listings"
 import Box from "../../components/Box"
 import Header from "../../components/Header"
@@ -9,32 +10,62 @@ const Listing = ({ listingData }) => {
     return (
         <>
             <Header />
-            <Box className="bg-gray-100 m-10 p-5">
-                <h1 className="px-5">{listingData.title}</h1>
-                <Box className=" m-5 p-2 w-2/3">
-                    <div className="relative flex-shrink-0 pb-1/2">
-                        <Image
-                            src={listingData.image}
-                            layout="fill"
-                            objectFit="cover"
-                            className="rounded-3xl"
-                        />
+            <Box className="bg-gray-100 mx-2 my-4 p-1 md:m-10 md:p-5">
+                <h1 className="text-3xl m-5 p-2 font-bold mt-2">
+                    {listingData.title}
+                </h1>
+                <div className="grid grid-cols-1 md:grid-cols-4">
+                    <div className="col-span-3">
+                        <Box className="m-5 p-1">
+                            <div className="relative flex-shrink-0 pb-1/2">
+                                <Image
+                                    src={listingData.image}
+                                    layout="fill"
+                                    objectFit="cover"
+                                    className="rounded"
+                                />
+                            </div>
+                        </Box>
+
+                        <Box className="bg-gray-50 m-5 p-3 px-6">
+                            <h1>Cijena: {listingData.price} kn</h1>
+                            <p>
+                                Cijena po m<sup>2</sup>:{" "}
+                                {Math.round(
+                                    listingData.price / listingData.size
+                                )}{" "}
+                                kn
+                            </p>
+                            <p>
+                                Stambena površina: {listingData.size} m
+                                <sup>2</sup>
+                            </p>
+                        </Box>
+                        <Box className="bg-gray-50 m-5 px-6 p-3">
+                            <p>
+                                Ukupna površina: {listingData.sizeTotal} m
+                                <sup>2</sup>
+                            </p>
+                        </Box>
+
+                        <div className="flex w-full">
+                            <Box className="bg-gray-50 flex w-full m-5 p-3 px-6">
+                                <p>{listingData.description}</p>
+                            </Box>
+                        </div>
                     </div>
-                </Box>
-                <div className="grid grid-cols-1 lg:grid-cols-2">
-                    <Box className="bg-gray-50 m-5 p-3 px-8 h-32">
-                        <h1>Cijena: {listingData.price} kn</h1>
-                    </Box>
-                    <Box className="bg-gray-50 m-5 px-8 p-3">
-                        <h1>
-                            Površina: {listingData.size} m<sup>2</sup>
-                        </h1>
-                    </Box>
-                </div>
-                <div className="flex w-full">
-                    <Box className="bg-gray-50 flex w-full m-5 p-3 px-8">
-                        <h1>{listingData.description}</h1>
-                    </Box>
+                    <div>
+                        <Box className="bg-gray-50 m-5 p-3 px-6 ">
+                            <div className="flex items-center space-x-3 py-2">
+                                <PhoneIcon className="h-4" />
+                                <p>{listingData.contactNumber}</p>
+                            </div>
+                            <div className="flex items-center space-x-3 py-2">
+                                <MailIcon className="h-4" />
+                                <p>{listingData.contactEmail}</p>
+                            </div>
+                        </Box>
+                    </div>
                 </div>
             </Box>
             <Footer />
