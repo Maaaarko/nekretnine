c30e4bbb20cf0fdb797377b7dd04fa90e1204ae8
Marko <marko.bolt4@gmail.com>
2021-12-24 22:38:17 +0100

    changed url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production"
 
-export const server = dev ? "http://localhost:3001" : "https://nekretnine.com"
+export const server = dev
+    ? "http://localhost:3001"
+    : "https://nekretnine-eta.vercel.app/"
