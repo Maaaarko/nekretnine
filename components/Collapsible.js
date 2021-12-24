e0225bbbd7a4cbdadbac76db9db6f2d613031e02
Marko <marko.bolt4@gmail.com>
2021-12-24 23:21:05 +0100

    visual changes to details

--- components/Collapsible.js
+++ components/Collapsible.js
@@ -13,7 +13,7 @@ const Collapsible = ({ children }) => {
             <div
                 className={`text-center ${
                     !isOpen
-                        ? "backdrop-blur-sm bg-gradient-to-b from-transparent to-gray-200 -mb-3 -mx-6 rounded-b-3xl"
+                        ? "backdrop-blur-sm bg-gradient-to-b from-transparent to-gray-200 -mb-4 pt-2 -mx-6 rounded-b-3xl"
                         : "-mb-2 mt-2"
                 }`}>
                 <button
